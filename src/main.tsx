54ff2e21f72f01aa139ec64b8d6423ab5d05f88f
hungpa1 <hungpa1@fpt.com>
2024-02-19 10:24:02 +0700

    FIX: Change font URL

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './fonts/SVN-BEBAS_NEUE_REGULAR.OTF'
-import './fonts/SVN-BEBAS_NEUE_BOLD.OTF'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
